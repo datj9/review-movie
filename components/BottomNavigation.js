b576eb0daf302dcd33343c9f44c6fdfee897ee96
tandat198 <tandat198@gmail.com>
2020-09-04 07:36:41 +0700

    update

--- components/BottomNavigation.js
+++ components/BottomNavigation.js
@@ -1,3 +1,12 @@
 import Link from "next/link";
 
-export default function BottomNavigation() {}
+export default function BottomNavigation() {
+    return (
+        <ul className='bottom-nav'>
+            <li className='nav-item'>Phim</li>
+            <li className='nav-item'>Rạp Chiếu</li>
+            <li className='nav-item'>Gần Đây</li>
+            <li className='nav-item'>Tài Khoản</li>
+        </ul>
+    );
+}
