1003730d7517be7e475a5cf286fc24c9e47f13e1
tandat198 <tandat198@gmail.com>
2020-09-20 09:22:25 +0700

    update

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,56 +1,58 @@
-// const aws = require("aws-sdk");
-// const multer = require("multer");
-// const multerS3 = require("multer-s3");
-// const path = require("path");
-// const { v4: uuidv4 } = require("uuid");
-// const bucket = process.env.BUCKET_NAME;
-// const accessKeyId = process.env.ACCESS_KEY_ID;
-// const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+const aws = require("aws-sdk");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const bucket = process.env.BUCKET_NAME;
+const accessKeyId = process.env.ACCESS_KEY_ID;
+const secretAccessKey = process.env.SECRET_ACCESS_KEY;
 
-// const s3 = new aws.S3({
-//     accessKeyId,
-//     secretAccessKey,
-// });
+const s3 = new aws.S3({
+    accessKeyId,
+    secretAccessKey,
+});
 
-// const upload = multer({
-//     storage: multerS3({
-//         s3,
-//         bucket,
-//         contentType: multerS3.AUTO_CONTENT_TYPE,
-//         acl: "public-read",
-//         metadata: function (req, file, cb) {
-//             cb(null, { fieldName: file.originalname });
-//         },
-//         key: function (req, file, cb) {
-//             const fileAfterSplited = file.originalname.split(".");
-//             const fileType = fileAfterSplited[fileAfterSplited.length - 1];
-//             cb(null, `/movies/images/${uuidv4()}.${fileType}`);
-//         },
-//     }),
-//     limits: {
-//         fileSize: 3 * Math.pow(1024, 2),
-//     },
-//     fileFilter: function (req, file, callback) {
-//         var ext = path.extname(file.originalname).toLowerCase();
-//         if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
-//             return callback(new Error("Only images are allowed"));
-//         }
-//         return callback(null, true);
-//     },
-// });
+const upload = multer({
+    storage: multerS3({
+        s3,
+        bucket,
+        contentType: multerS3.AUTO_CONTENT_TYPE,
+        acl: "public-read",
+        metadata: function (req, file, cb) {
+            cb(null, { fieldName: file.originalname });
+        },
+        key: function (req, file, cb) {
+            const fileAfterSplited = file.originalname.split(".");
+            const fileType = fileAfterSplited[fileAfterSplited.length - 1];
+            cb(null, `/news/images/${uuidv4()}.${fileType}`);
+        },
+    }),
+    limits: {
+        fileSize: 4 * Math.pow(1024, 2),
+    },
+    fileFilter: function (req, file, callback) {
+        const ext = path.extname(file.originalname).toLowerCase();
 
-// const uploadSingle = (type) => (req, res) => {
-//     return upload.single(type)(req, res, function (err) {
-//         if (err instanceof multer.MulterError) {
-//             [("name", "storageErrors")].forEach((e) => delete err[e]);
-//             return res.status(400).json({ error: err });
-//         } else if (err) {
-//             console.log(err);
-//             return res.status(400).json({ error: err });
-//         } else {
-//             return res.status(200).json({ linkUrl: req.file.location });
-//         }
-//     });
-// };
+        if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+            return callback(new Error("Only images are allowed"));
+        }
 
-// module.exports = { uploadSingle };
+        return callback(null, true);
+    },
+});
+
+const uploadSingle = (type) => (req, res) => {
+    return upload.single(type)(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            [("name", "storageErrors")].forEach((e) => delete err[e]);
+            return res.status(400).json({ error: err });
+        } else if (err) {
+            console.log(err);
+            return res.status(400).json({ error: err });
+        } else {
+            return res.status(200).json({ linkUrl: req.file.location });
+        }
+    });
+};
+
+module.exports = { uploadSingle };
--- routes/api/news/index.js
+++ routes/api/news/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const newsController = require("./controller");
 const { authenticate, authorize } = require("../../../middlewares/auth");
+const { uploadSingle } = require("../../../middlewares/upload");
 
 router.get("/", newsController.getNews);
 router.post("/", authenticate, authorize(["admin"]), newsController.createNews);
+router.post("/upload-image", uploadSingle("image"));
 
 module.exports = router;
